486923e33e7b6f445229a117c6aeb3eb26f73ed4
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-01-27 19:18:17 -0300

    Inseridos comentários

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+/**
+ * Esse é o arquivo inicial a ser executado
+ */
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  /**
+   * <React.StrictMode> -> em modo de desenvolvimento ajuda a previnir erros.
+   */
   <React.StrictMode>
     <App />
   </React.StrictMode>
