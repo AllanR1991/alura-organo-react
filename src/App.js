d57d6fb9cdc992251c7091b5ab58f51629611d94
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-01-28 19:08:42 -0300

    Removido os importes desnecessários e corrigio o do formulário

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import Banner from "./components/Banner";
 /**
  * Para evitar como foi feito no import do banner e chamar 2x o nome banner, precisamos mudar o nome do arquivo JS do componente para index ai so precisamos passar o caminho da pasta que o JS faz o resto para nós.
  */
-import CampoTexto from "./components/CampoTexto";
-import FormularioColaborador from "./components/FormularioColaborador";
+
+import FormularioColaborador from "./components/Formulario";
 
 function App() {
   return (
