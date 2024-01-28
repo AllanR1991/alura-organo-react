be97be5359dde9383a47420de0142d33788b8b7b
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-01-28 20:05:01 -0300

    Removemos a props.Texto para utilizar props.children no componente botão

--- src/components/Botao/index.js
+++ src/components/Botao/index.js
@@ -2,7 +2,8 @@ import "./Botao.css"
 
 const Botao = (props) => {
     return(
-        <button className="botao">{props.texto}</button>
+        // Estamos obtendo os filhos do botao como propriedade, outra forma de fazer
+        <button className="botao">{props.children}</button>
     );
 };
 
