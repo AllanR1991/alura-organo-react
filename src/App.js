d3dea4a135766274072c7e5f2d9cdf83247b69d1
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-01-27 21:56:10 -0300

    Retirado os campos texto e realocado dentro do componente FormularioColaborador

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import Banner from "./components/Banner";
  * Para evitar como foi feito no import do banner e chamar 2x o nome banner, precisamos mudar o nome do arquivo JS do componente para index ai so precisamos passar o caminho da pasta que o JS faz o resto para nós.
  */
 import CampoTexto from "./components/CampoTexto";
+import FormularioColaborador from "./components/FormularioColaborador";
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-      <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
-      <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"/>
+      <FormularioColaborador/>
     </div>
   );
 }
