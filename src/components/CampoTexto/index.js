2c705c284ad3ff5bb73293cfb7309c57f6190fab
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-01-27 21:43:10 -0300

    1ª utilização de props, tornando o componete reutilizavel

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,10 +1,18 @@
 import "./CampoTexto.css"
 
-const CampoTexto = () => {
+const CampoTexto = (props) => {
+    //React nos entrega implicitamente um parâmetro chamado props, que são as propriedades que esse componente recebeu
+
+
+    //console.log(props.label);
+    // Utilizando o Console.log cima, o console.log é duplicado 2x devido ao <React.StrictMode> no index.js na pasta src, pois ele renderiza 2x para validações, porem so ocorre no modo desenvolvimento.
+
+    //const placeholderModificada = `${props.placeholder} ...`
+
     return (
         <div className="campo-texto">
-            <label>Nome</label>
-            <input placeholder="Digite seu nome"/>
+            <label>{props.label}</label>
+            <input placeholder={props.placeholder}/>
         </div>
     );
 };
