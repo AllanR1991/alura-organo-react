dfa53ef2cc2964c80c16eade2b3f8cb3a919769c
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-02-04 10:06:05 -0300

    Criado useState para setar colaboradores, criadofunção aoNovoColaboradorAdicionado

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Banner from "./components/Banner";
 /**
  * Para evitar como foi feito no import do banner e chamar 2x o nome banner, precisamos mudar o nome do arquivo JS do componente para index ai so precisamos passar o caminho da pasta que o JS faz o resto para nós.
@@ -6,10 +7,19 @@ import Banner from "./components/Banner";
 import FormularioColaborador from "./components/Formulario";
 
 function App() {
+
+  const[colaboradores, setColaboradores] = useState([]);
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    //Pega os colaboradores que ja tinha e e adiciona mais um.
+    setColaboradores([...colaboradores, colaborador])
+  } 
+
   return (
     <div className="App">
       <Banner/>
-      <FormularioColaborador/>
+      <FormularioColaborador aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
     </div>
   );
 }
